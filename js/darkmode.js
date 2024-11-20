67b1df4d7243f2a0e259c4d1db8eb60f70287423
css pro <qwertyum4675@proton.me>
2024-11-20 11:33:04 -0800

    mb gang im stupid

--- js/darkmode.js
+++ js/darkmode.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
             const givemeheader = document.querySelector('header');
             givemeheader.style.backgroundColor = '#00c0bf';
             const repoHref = document.getElementById('repomydepot');
-            repoHref.text-shadow = '0 0 3px #FF0000, 0 0 5px #0000FF';
+            repoHref.style.backgroundColor = '#33ff66';
             
             const foot = document.querySelector('.footer');
             foot.style.backgroundColor = '#010101';
